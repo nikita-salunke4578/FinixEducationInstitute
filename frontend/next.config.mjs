0f438ff2d444230e31c07643f38f09c6b20823a5
nikita-salunke4578 <nikitasalunke960@gmail.com>
2026-05-02 01:01:54 +0530

    fix: added DB_PORT configuration to fix Render DB timeout

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*', // Proxy to Backend
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/:path*`, // Proxy to Backend
       },
     ]
   },
